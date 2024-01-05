5f39473ad0d0635d5d96fd0bf6811fccc76e800c
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-01-06 00:19:02 +0530

    updated

--- src/Components/Arena.jsx
+++ src/Components/Arena.jsx
@@ -15,65 +15,65 @@ const Arena = () => {
         <div className="photo-grid-container">
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1515266591878-f93e32bc5937?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsdWV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1587590227264-0ac64ce63ce8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1520121401995-928cd50d4e27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlZW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHVycGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3JhbmdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1520338258525-606b90f95b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRhcmslMjBibHVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1521127474489-d524412fd439?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
           <div className="photo-grid-item">
             <img className="" src="https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70" />
-            <div className="flex justify-between items-center">
-              <div className="text-2xl">(2020)</div>
-              <div>Poster design for an event</div>
+            <div className="flex justify-between">
+              <div className="">(2020)</div>
+              <div className="">Poster</div>
             </div>
           </div>
         </div>
