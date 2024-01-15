c32c253507c03ce32419776632cad3736f12357c
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-01-15 06:34:02 +0530

    under maintenence

---
+++ src/Components/Offline.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Offline = () => {
+  return (
+    <div className='h-screen flex items-center justify-center bg-neutral-900 text-white text-4xl'>Offline</div>
+  )
+}
+
+export default Offline
--- src/main.jsx
+++ src/main.jsx
@@ -8,39 +8,41 @@ import Arena from "./Components/Arena";
 import Work from "./Components/Work";
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout.jsx';
+import Offline from './Components/Offline';
 
-const router=createBrowserRouter([
-    {
-        path: '/',
-        element: <Layout/>,
-        children: [
-            {
-                path: "",
-                element: <Home/>,
-            },
-            {
-                path: "work",
-                element: <Work/>,
-            },
-            {
-                path: "arena",
-                element: <Arena/>,
-            },
-            {
-                path: "about",
-                element: <About/>,
-            },
-            {
-                path: "contact",
-                element: <Contact/>,
-            },
+// const router=createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <Layout/>,
+//         children: [
+//             {
+//                 path: "",
+//                 element: <Home/>,
+//             },
+//             {
+//                 path: "work",
+//                 element: <Work/>,
+//             },
+//             {
+//                 path: "arena",
+//                 element: <Arena/>,
+//             },
+//             {
+//                 path: "about",
+//                 element: <About/>,
+//             },
+//             {
+//                 path: "contact",
+//                 element: <Contact/>,
+//             },
             
-        ]
-    }
-])
+//         ]
+//     }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        {/* <RouterProvider router={router}/> */}
+        <Offline/>
     </React.StrictMode>
 )
