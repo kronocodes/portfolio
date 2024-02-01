4732b7c1cf50844134488ab2c8add6075d4f6caf
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-02-02 03:23:37 +0530

    push

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -2,18 +2,46 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <>
+    <div className=''>
       <div className='font-semibold text-5xl'>I WEAR A LOT OF HATS</div>
-      <div class="container mx-auto">
+      <div class="px-5 ">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-0">
-          <div class="flex justify-center text-2xl border-2 border-gray-400">Beautiful UI</div>
-          <div class="flex justify-center text-2xl border-2 border-gray-400">Efficient UX</div>
-          <div class="flex justify-center text-2xl border-2 border-gray-400">Fullstack Web Development</div>
-          <div class="flex justify-center text-2xl border-2 border-gray-400">Software Development</div>
+          <div class="flex flex-col justify-center border-2 border-gray-400 px-4 lg:px-9 ">
+            <div className='pt-2 -ml-2'><img className='w-14' src="https://assets-global.website-files.com/6547600aa272d0cc570e8feb/654f6f6decb3359349b05046_View-all.svg" alt="ar" /></div>
+            <div className='flex flex-wrap justify-between py-1'>
+            <div className='text-3xl font-semibold'>Software Developer</div>
+            <div className='bg-neutral-300 w-fit h-fit text-center px-4 py-1 mt-1 border-2 border-zinc-400 rounded-md text-sm'>14 years</div>
+            </div>
+            <div className='pb-9 pt-3'>14 years in design, helping my client's and direct stakeholders in raising over $350M, going IPO and reaching 200M+ users. I merge great aesthetics, design thinking with strategic growth tactics.</div>
+          </div>
+          <div class="flex flex-col justify-center border-2 border-gray-400 px-4 lg:px-9 ">
+            <div className='pt-2 -ml-2'><img className='w-14' src="https://assets-global.website-files.com/6547600aa272d0cc570e8feb/654f6f6decb3359349b05046_View-all.svg" alt="ar" /></div>
+            <div className='flex flex-wrap justify-between py-1'>
+            <div className='text-3xl font-semibold'>Designer</div>
+            <div className='bg-neutral-300 w-fit h-fit text-center px-4 py-1 mt-1 border-2 border-zinc-400 rounded-md text-sm'>14 years</div>
+            </div>
+            <div className='pb-9 pt-3'>14 years in design, helping my client's and direct stakeholders in raising over $350M, going IPO and reaching 200M+ users. I merge great aesthetics, design thinking with strategic growth tactics.</div>
+          </div>
+          <div class="flex flex-col justify-center border-2 border-gray-400 px-4 lg:px-9 ">
+            <div className='pt-2 -ml-2'><img className='w-14' src="https://assets-global.website-files.com/6547600aa272d0cc570e8feb/654f6f6decb3359349b05046_View-all.svg" alt="ar" /></div>
+            <div className='flex flex-wrap justify-between py-1'>
+            <div className='text-3xl font-semibold'>Web Developer</div>
+            <div className='bg-neutral-300 w-fit h-fit text-center px-4 py-1 mt-1 border-2 border-zinc-400 rounded-md text-sm'>14 years</div>
+            </div>
+            <div className='pb-9 pt-3'>14 years in design, helping my client's and direct stakeholders in raising over $350M, going IPO and reaching 200M+ users. I merge great aesthetics, design thinking with strategic growth tactics.</div>
+          </div>
+          <div class="flex flex-col justify-center border-2 border-gray-400 px-4 lg:px-9 ">
+            <div className='pt-2 -ml-2'><img className='w-14' src="https://assets-global.website-files.com/6547600aa272d0cc570e8feb/654f6f6decb3359349b05046_View-all.svg" alt="ar" /></div>
+            <div className='flex flex-wrap justify-between py-1'>
+            <div className='text-3xl font-semibold'>Founder</div>
+            <div className='bg-neutral-300 w-fit h-fit text-center px-4 py-1 mt-1 border-2 border-zinc-400 rounded-md text-sm'>14 years</div>
+            </div>
+            <div className='pb-9 pt-3'>14 years in design, helping my client's and direct stakeholders in raising over $350M, going IPO and reaching 200M+ users. I merge great aesthetics, design thinking with strategic growth tactics.</div>
+          </div>
         </div>
       </div>
       <div>YUP, I DO'EM ALL!</div>
-    </>
+    </div>
   )
 }
 
