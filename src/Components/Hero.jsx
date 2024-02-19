85a75d6d9fbdd979f0247ff10fac9c095da387e9
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-02-19 06:27:27 +0530

    ok

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
         <div className='bg-zinc-800 w-full h-full mt-8 mx-3 border border-zinc-600 rounded-2xl gradient-box relative'>
           {/* <div className='w-[15%] rounded-2xl opacity-40 absolute bg-gradient-to-l from-transparent to-black left-0 h-full'></div> */}
           {/* <div className='w-[15%] rounded-2xl opacity-40 absolute bg-gradient-to-l from-black to-transparent right-0 h-full'></div> */}
-          <div className='h-[25%] rounded-2xl opacity-40 absolute bg-gradient-to-t from-transparent to-black top-0 w-full'></div>
-          <div className='h-[25%] rounded-2xl opacity-40 absolute bg-gradient-to-b from-transparent to-black bottom-0 w-full'></div>
-          <div className=' text-white px-10 mx-auto max-w-[1220px] mt-[240px] text-[25px] leading-[33px] md:text-[40px] md:leading-[54px] md:px-24  lg:mt-[260px] lg:text-[45px] lg:leading-[70px]'>
+          {/* <div className='h-[25%] rounded-2xl opacity-40 absolute bg-gradient-to-t from-transparent to-black top-0 w-full'></div>
+          <div className='h-[25%] rounded-2xl opacity-40 absolute bg-gradient-to-b from-transparent to-black bottom-0 w-full'></div> */}
+          <div className=' text-white px-10 mx-auto max-w-[1220px] mt-[260px] text-[25px] leading-[33px] md:text-[40px] md:leading-[54px] md:px-24  lg:mt-[260px] lg:text-[45px] lg:leading-[70px]'>
               <div class="flex flex-wrap gap-x-2 ">
                 <div>Hello!</div>
                 <div class='xl:h-11 xl:mt-3 xl:w-24 md:w-16 md:h-10 md:mt-2 h-6 w-10 mt-1'>
