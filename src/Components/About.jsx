8fbf996a92095e85e80f85941c30f9263fa6ed54
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-02-28 10:12:06 +0530

    ok

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -19,13 +19,18 @@ const About2 = () => {
   return (
     <div className="bg-black flex flex-col items-center gap-y-44 text-white">
       <div className="2xl:max-w-[1300px] md:px-16 lg:max-w-[1000px] gap-y-14 w-full py-36 flex flex-col">
-
-
         <div className="flex flex-col md:h-[420px] md:flex-row md:gap-x-8 px-4 md:px-0 gap-y-5">
           <div className="xl:px-12 md:px-8 px-6 flex flex-col xl:gap-y-6 md:gap-y-2 gap-y-2 pt-7 border border-zinc-600 bg-zinc-900 rounded-3xl">
-            <div className="pt-4 md:text-2xl text-xl">Hey! I'm Abhishek Nandan</div>
-            <div className="xl:text-5xl md:text-4xl text-3xl font-medium">Passionate to pursue the technology.</div>
-            <div className="xl:w-full pb-10 md:pb-0 md:text-lg text-sm text-zinc-400">I enjoy learning new things and try to overcome new challenges while analyzing how I improved through them.</div>
+            <div className="pt-4 md:text-2xl text-xl">
+              Hey! I'm Abhishek Nandan
+            </div>
+            <div className="xl:text-5xl md:text-4xl text-3xl font-medium">
+              Passionate to pursue the technology.
+            </div>
+            <div className="xl:w-full pb-10 md:pb-0 md:text-lg text-sm text-zinc-400">
+              I enjoy learning new things and try to overcome new challenges
+              while analyzing how I improved through them.
+            </div>
           </div>
           <div className="md:min-w-fit md:h-[420px] h-[400px] flex justify-center  md:border border-zinc-600 rounded-3xl">
             <img
@@ -39,11 +44,12 @@ const About2 = () => {
         {/* <div><Leetcode/></div> */}
         {/* <div><Calendar/></div> */}
 
-
-        <div className="flex flex-col justify-center min-w-xl md:max-w-[50%] px-4 md:px-0">
+        {/* <div className="flex flex-col justify-center min-w-xl md:max-w-[50%] px-4 md:px-0">
           <div className="flex items-center pt-5 md:px-4 px-2 md:pl-7">
             <div class="w-3 h-3 rounded-full bg-white glow"></div>
-            <div className="md:px-4 pl-3 text-sm text-zinc-300">MY S T O R Y</div>
+            <div className="md:px-4 pl-3 text-sm text-zinc-300">
+              MY S T O R Y
+            </div>
           </div>
 
           <div className="flex justify-between md:pr-4 pb-2">
@@ -57,7 +63,7 @@ const About2 = () => {
           <div className="w-full aspect-[2/3] bg-zinc-600 p-2 border border-zinc-400 rounded-2xl">
             <Carousel images={images} />
           </div>
-        </div>
+        </div> */}
 
         <div className="border border-zinc-600 md:p-2 py-2 bg md:rounded-2xl">
           <Education />
@@ -67,27 +73,32 @@ const About2 = () => {
           <Services />
         </div>
         <div className="border border-zinc-600 md:p-2 py-2 bg md:rounded-2xl">
-        <Skills/>
+          <Skills />
         </div>
         {/* Spotify playlist */}
-        <div className="bg-zinc-800 border border-zinc-600 md:rounded-2xl">
-        <div className='flex items-center px-6 pt-5'>
-        <div class="w-3 h-3 rounded-full bg-white glow z-10"></div>
-        <div className='px-4 text-sm text-zinc-300'>M Y &nbsp; C O D I N G &nbsp; P L A Y L I S T</div>
-        </div>
+        {/* <div className="bg-zinc-800 border border-zinc-600 md:rounded-2xl">
+          <div className="flex items-center px-6 pt-5">
+            <div class="w-3 h-3 rounded-full bg-white glow z-10"></div>
+            <div className="px-4 text-sm text-zinc-300">
+              M Y &nbsp; C O D I N G &nbsp; P L A Y L I S T
+            </div>
+          </div>
           <iframe
-          
             className="w-full h-[44vh] "
             src="https://open.spotify.com/embed/playlist/0oRActjFDEhtOG8eCteVkC?utm_source=generator&theme=0"
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
           />
-        </div>
-        <div className="text-center py-10">
-          <div className="md:text-3xl text-2xl font-semibold">BECAUSE THERE'S MUCH MORE</div>
-          <div className="md:text-3xl text-2xl font-semibold pb-10">TO LIFE THAN PUSHING PIXELS</div>
-          <Card/>
-        </div>
+        </div> */}
+        {/* <div className="text-center py-10">
+          <div className="md:text-3xl text-2xl font-semibold">
+            BECAUSE THERE'S MUCH MORE
+          </div>
+          <div className="md:text-3xl text-2xl font-semibold pb-10">
+            TO LIFE THAN PUSHING PIXELS
+          </div>
+          <Card />
+        </div> */}
       </div>
     </div>
   );
