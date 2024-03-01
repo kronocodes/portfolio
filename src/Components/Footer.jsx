7e811e5818e2b217b5513e81cbdd10d57cac5bc5
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-03-01 22:19:31 +0530

    o

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -16,15 +16,15 @@ const Footer = () => {
     <div className='flex justify-center bg-black text-white'>
       <div className="max-w-[1300px] rounded-t-2xl bg-gradient-to-b to-black from-neutral-800 mt-5 w-full px-4 mx-4 ">
         <div className="py-10">
-          <div className="flex gap-x-10 md:gap-x-20 justify-center pl-1 text-xl">
-              <a className="hover:bg-neutral-600 transition-all duration-500 hover:px-5 py-2 rounded-xl" href="https://github.com/kronocodes">Github</a>
-              <a className="hover:bg-neutral-600 transition-all duration-500 hover:px-5 py-2 rounded-xl" href="https://www.linkedin.com/in/abhisheknandan2013/">LinkedIn</a>
-              <a className="hover:bg-neutral-600 transition-all duration-500 hover:px-5 py-2 rounded-xl" href="https://twitter.com/RakNandan">Twitter</a>
-              <button onClick={handleCopy} className="hover:bg-neutral-600 transition-all duration-500 hover:px-5 py-2 rounded-xl">Email</button>
+          <div className="flex gap-x-10 md:gap-x-12 justify-center text-xl">
+              <a className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl" href="https://github.com/kronocodes">Github</a>
+              <a className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl" href="https://www.linkedin.com/in/abhisheknandan2013/">LinkedIn</a>
+              <a className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl" href="https://twitter.com/RakNandan">Twitter</a>
+              <button onClick={handleCopy} className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl">Email</button>
           </div>
         </div>
         <div>
-          <div className='text-center text-[21px] md:text-3xl italic pb-10'>Made with love, peer pressure & react</div>
+          <div className='text-center text-[21px] md:text-2xl italic pb-10'>Made with love, peer pressure & react</div>
         </div>
         <div className="bg-neutral-600 h-[1px]"></div>
         <div className="flex sm:justify-between justify-center  py-4 text-sm">
