f24c32b768356a4ca135239018a8d0dade953956
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-05-08 01:43:25 +0530

    OK

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
       <div className='flex items-center justify-center bg-black z-10'>
         <div className='w-full mt-2 mx-2 h-[98vh] border border-zinc-600 rounded-xl gradient-box flex justify-center items-center'>
           <div className='text-zinc-400 px-5 sm:px-10 mx-auto md:px-20 max-w-[1000px] text-[40px] leading-[60px] md:text-[64px] md:leading-[90px] lg:text-[74px] lg:leading-[100px] font-light'>
-              <div class="flex flex-wrap gap-x-2 md:gap-x-4 xl:gap-x-6">
+              <div class="flex fo flex-wrap gap-x-2 md:gap-x-4 xl:gap-x-6">
                 <div>Hey,</div>
                 <div>I'm</div>
                 <div class='lg:h-20 lg:w-32 md:w-24 md:h-16 h-10 w-14 my-auto'>
@@ -84,7 +84,7 @@ const Hero = () => {
                       <path d="M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z" />
                       </svg>
                     </span>
-                    <span class="magic-text">designer</span>
+                    <span class="magic-text design">designer</span>
                   </span>
                 </h1>
                 <div>based</div>
