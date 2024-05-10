9aecaa3abfd651916816ce2033365f084d7a5554
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-05-10 17:16:34 +0530

    ok

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -16,24 +16,28 @@ const Footer = () => {
     <div className='flex justify-center bg-black text-white'>
       <div className="md:rounded-t-2xl bg-gradient-to-b from-black to-neutral-800 mt-5 w-full px-4 md:mx-4 ">
         <div className="py-10">
-          <div className="flex gap-x-8 md:gap-x-12 justify-center text-xl">
-              <a className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl" href="https://github.com/kronocodes">Github
-              <img className='w-7 hover:flex' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img>
-              </a>
-              <a className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl" href="https://www.linkedin.com/in/abhisheknandan2013/">LinkedIn
-              <img className='w-7 hover:flex' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img>
-              </a>
-              <a className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl" href="https://twitter.com/RakNandan">Twitter
-              <img className='w-7 hover:flex' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img>
-              </a>
-              <button onClick={handleCopy} className="hover:bg-neutral-600 transition-all duration-300 hover:px-5 py-2 rounded-xl">Email
-              <img className='w-7 hover:flex' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img>
+          <div className="flex md:gap-x-2 justify-center text-xl">
+              <div className="hover:bg-neutral-600 group flex items-center transition-all duration-300 hover:pl-4 hover:pr-2 py-2 rounded-xl" href="https://github.com/kronocodes">
+                <div>Github</div>
+                <img className='opacity-0 pl-2 group-hover:opacity-100 transition-opacity' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img>
+              </div>
+              <div className="hover:bg-neutral-600 group flex items-center transition-all duration-300 hover:pl-4 hover:pr-2 py-2 rounded-xl" href="https://www.linkedin.com/in/abhisheknandan2013/">
+                <div>LinkedIn</div>
+                <img className='opacity-0 pl-2 group-hover:opacity-100 transition-opacity' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img>
+              </div>
+              <div className="hover:bg-neutral-600 group flex items-center transition-all duration-300 hover:pl-4 hover:pr-2 py-2 rounded-xl" href="https://twitter.com/RakNandan">
+                <div>Twitter</div>
+                <img className='opacity-0 pl-2 group-hover:opacity-100 transition-opacity' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img>
+              </div>
+              <button onClick={handleCopy} className="hover:bg-neutral-600 group flex items-center transition-all duration-300 hover:px-4 py-2 rounded-xl">
+                <div>Email</div>
+                {/* <img className='opacity-0 pl-2 group-hover:opacity-100 transition-opacity' src='https://assets-global.website-files.com/63dcb6e1a80e9454b630f4c4/641284285486aaab07feafaa_icon-arrow-project.svg'></img> */}
               </button>
               
           </div>
         </div>
         <div>
-          <div className='text-center text-[21px] md:text-2xl italic pb-10'>Made with love, peer pressure & react</div>
+          <div className='text-center text-[20px] md:text-2xl italic pb-10'>Made with love, peer pressure & react</div>
         </div>
         <div className="bg-neutral-600 h-[1px]"></div>
         <div className="flex md:justify-between justify-center  py-4 text-sm">
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -50,7 +50,7 @@ const Hero = () => {
   return (
     <>
       <div className='flex items-center justify-center bg-black z-10'>
-        <div className='w-full mt-2 mx-2 h-[98vh] border border-zinc-600 rounded-xl gradient-box flex justify-center items-center'>
+        <div className='w-full mt-2 mx- h-[98vh] border border-zinc-600 rounded-xl gradient-box flex justify-center items-center'>
           <div className='text-zinc-400 px-5 sm:px-10 mx-auto md:px-20 max-w-[1000px] text-[40px] leading-[60px] md:text-[64px] md:leading-[90px] lg:text-[74px] lg:leading-[100px] font-light'>
               <div class="flex customfont flex-wrap gap-x-2 md:gap-x-4 xl:gap-x-6">
                 <div>Hey,</div>
@@ -58,8 +58,8 @@ const Hero = () => {
                 <div class='lg:h-20 lg:w-32 md:w-24 md:h-16 h-10 w-14 my-auto'>
                   <img className='h-full w-full object-cover lg:rounded-[22px] rounded-[10px]' src={im}></img>
                 </div>
-                <div className='text-white'>Abhishek</div>
-                <div className='sm:hidden flex'>, a</div>
+                <div className='text-white'>Abhishek,</div>
+                <div className='sm:hidden flex'>a</div>
                 <div className='flex flex-wrap gap-x-2 md:gap-x-4 xl:gap-x-6'>
                 <div className='hidden sm:flex'>a</div>
                 <div className='text-white'>software developer</div>
