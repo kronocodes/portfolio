59c0e928d9db52ae17473a331787e04e16c326f2
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-06-25 04:07:03 +0530

    analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,12 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import { Outlet } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 function Layout () {
   return (
     <>
+      <Analytics/>
       <Header/>
       <Outlet/>
       <Footer/>
