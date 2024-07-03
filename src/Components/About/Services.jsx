45c8b36fae81ec8c002f71223dad859c98e89264
Abhishek Nandan <135464165+kronocodes@users.noreply.github.com>
2024-07-04 05:28:20 +0530

    ok

--- src/Components/About/Services.jsx
+++ src/Components/About/Services.jsx
@@ -51,14 +51,12 @@ const Services = () => {
           <div className='px-4 text-zinc-300'>S E R V I C E S</div>
         </div>
         <div className="md:px-4 px-2 pt-8">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="flex flex-col gap-6">
             {servicesData.map((service, index) => (
-              <div key={index} className="flex flex-col md:rounded-xl md:hover:translate-x-2 transition duration-200 md:border-[1px] border-zinc-800 pl-4 pr-5 lg:px-9">
-                <div className='pt-1 -ml-3 md:flex hidden'>
-                  <img className='w-12' src={service.imageSrc} alt="service icon" />
-                </div>
-                <div className='flex flex-wrap justify-between md:py-1 pt-4'>
-                  <div className='md:text-3xl text-2xl font-semibold'>{service.title}</div>
+              <div key={index} className="flex flex-col md:rounded-xl md:hover:translate-x-2 transition duration-200 pl-4 pr-5 lg:px-9">
+                <div className='flex'>
+                  <img className='w-12 md:flex hidden -ml-3' src={service.imageSrc} alt="service icon" />
+                  <div className='md:text-3xl text-2xl font-semibold md:py-1 pt-4 md:pl-1'>{service.title}</div>
                 </div>
                 <div className={`md:pb-8 pb-2 pt-3 text-zinc-300 ${visibleDescriptionIndex === index ? 'max-h-none' : 'max-h-10 overflow-hidden relative md:max-h-none md:overflow-visible'}`}>
                   {service.description}
@@ -76,9 +74,9 @@ const Services = () => {
                     <svg fill="#dedede" height="10px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330.00 330.00" xml:space="preserve" stroke="#fafafa" stroke-width="0.0033" transform="matrix(1, 0, 0, -1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="1.98"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394 l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393 C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"></path> </g></svg>
                   }
                 </button>
-                {/* {service.title != "UI/UX Designer" && (
-                    <div className="bg-zinc-700 w-full h-[1px] md:hidden"></div>
-                  )} */}
+                {service.title != "UI/UX Designer" && (
+                    <div className="bg-zinc-700 w-full h-[1px] hidden md:flex"></div>
+                  )}
               </div>
               
             ))}
